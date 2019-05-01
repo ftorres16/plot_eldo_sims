08f6e9f72edf9b9e46de16c0e1426e6bae06aab1
fabian <fabian.torres.alvarez+github@gmail.com>
2019-05-01 19:28:25 -0300

    now getting all traces from the file

--- plot_eldo_sims/cli.py
+++ plot_eldo_sims/cli.py
@@ -1,10 +1,50 @@
 import click
+import matplotlib.pyplot as plt
 
 
 @click.command()
 @click.argument("input", type=click.File("r"))
 def cli(input):
-    pass
+    plot_flag = False
+    data_flag = False
+    counter = 0
+
+    all_traces = []
+
+    for line in input.readlines():
+        if "TRANSIENT ANALYSIS" in line:
+            plot_flag = True
+            counter = 0
+            continue
+
+        if plot_flag and counter < 5:
+            counter += 1
+            continue
+
+        if plot_flag and counter == 5:
+            headers = line.split()
+            trace = {header: [] for header in headers}
+
+            plot_flag = False
+            data_flag = True
+            counter = 0
+
+        if data_flag and counter < 3:
+            counter += 1
+            continue
+
+        if data_flag and counter == 3:
+            if 'Y' in line:
+                data_flag = False
+                counter = 0
+                all_traces.append(trace)
+                continue
+            else:
+                values = line.split()
+                for header, value in zip(headers, values):
+                    trace[header].append(float(value))
+
+    print(all_traces)
 
 
 if __name__ == "__main__":
