e78a49fe9cdfc924e4246a1ee43ef85c868ac9f5
fabian <fabian.torres.alvarez+github@gmail.com>
2019-05-04 13:05:16 -0300

    added support for ac simulations

--- plot_eldo_sims/cli.py
+++ plot_eldo_sims/cli.py
@@ -9,15 +9,21 @@ from matplotlib.ticker import EngFormatter
 def cli(input):
     tran_flag = False
     dc_flag = False
+    ac_flag = False
     all_traces = {
         "tran": {"V": {"unit": "V", "traces": []}, "I": {"unit": "A", "traces": []}},
-        "dc": {"V": {"unit": "V", "traces": []}, "I": {"unit": "A", "traces": []}}
+        "dc": {"V": {"unit": "V", "traces": []}, "I": {"unit": "A", "traces": []}},
+        "ac": {
+            "Mag": {"unit": "db", "traces": []},
+            "Phase": {"unit": "°", "traces": []},
+        },
     }
 
     for line in input.readlines():
         if "TRANSIENT ANALYSIS" in line:
             tran_flag = True
             dc_flag = False
+            ac_flag = False
             data_flag = False
             print_legends = {}
             continue
@@ -25,6 +31,14 @@ def cli(input):
         elif "DC TRANSFER CURVES" in line:
             tran_flag = False
             dc_flag = True
+            ac_flag = False
+            data_flag = False
+            print_legends = {}
+
+        elif "AC ANALYSIS" in line:
+            tran_flag = False
+            dc_flag = False
+            ac_flag = True
             data_flag = False
             print_legends = {}
 
@@ -80,6 +94,37 @@ def cli(input):
                     for header, value in zip(headers, values):
                         trace[header].append(float(value))
 
+        elif ac_flag:
+            if "Print_Legend" in line:
+                legend = line.split()[-1]
+                header_name = line.split()[1][:-1]
+                print_legends[header_name] = legend
+
+            elif "HERTZ" in line:
+                headers = [print_legends.get(header, header) for header in line.split()]
+                trace = {header: [] for header in headers}
+
+                data_flag = True
+                counter = 0
+
+            elif data_flag and counter < 2:
+                counter += 1
+
+            elif data_flag and counter == 2:
+                if "Y" in line:
+                    ac_flag = False
+                    trace_type = headers[-1].split("(")[0][1:]
+                    if trace_type == "DB":
+                        trace_type = "Mag"
+                    elif trace_type == "P":
+                        trace_type = "Phase"
+
+                    all_traces["ac"][trace_type]["traces"].append(trace)
+                else:
+                    values = line.split()
+                    for header, value in zip(headers, values):
+                        trace[header].append(float(value))
+
     print(all_traces)
 
     if all_traces["tran"]:
@@ -127,7 +172,35 @@ def cli(input):
                 ax.minorticks_on()
                 ax.legend()
 
-        plt.show()
+    if all_traces["ac"]:
+        for label, plot in all_traces["ac"].items():
+            if not plot["traces"]:
+                continue
+
+            fig, ax = plt.subplots()
+            for trace in plot["traces"]:
+                signals = [key for key in trace.keys() if key != "HERTZ"]
+
+                for signal in signals:
+                    label = (
+                        signal.replace("(", " ")
+                        .replace(")", " ")
+                        .replace(",", "-")
+                        .split()[-1]
+                    )
+                    ax.plot(trace["HERTZ"], trace[signal], label=label)
+
+                plt.ylabel(f"{label} ({plot['unit']})")
+                plt.xlabel("Freq (Hz)")
+                ax.set_xscale('log')
+                ax.xaxis.set_major_formatter(EngFormatter())
+                plt.title("AC Analysis")
+                ax.grid(True, which="major")
+                ax.grid(True, which="minor", linestyle=":")
+                ax.minorticks_on()
+                ax.legend()
+
+    plt.show()
 
 
 if __name__ == "__main__":
